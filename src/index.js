34f045c44c7680c5ea49d01f0fa3e55d3a017267
Kashish Bansal <115978805+kashishbansal081@users.noreply.github.com>
2025-08-28 12:06:20 +0530

    Update index.js
    removed app.css imported line

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./app.css"
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import CardDetail from "./components/CardDetail";
 
